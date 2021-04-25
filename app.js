3b05c0b90a90b949d3f0c74902cab1bb4ab1c304
Akhil Goyal <akhilgoyal@macbook-pro.lan>
2021-04-25 18:11:58 -0400

    Implemented fetching product by ID.

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const mysql = require('mysql');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const config = require('./config');
+const path = require('path');
 
 const app = express();
 const router = express.Router();
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,20 +20,12 @@ const connection = mysql.createConnection(config.dbConfig);
 connection.connect(err => {
     if (err) throw err;
     console.log("Connected to DB!");
-
-    // Query to display all tables in database.
-    connection.query("SHOW TABLES", (err, results) => {
-        if (err) throw err;
-        // console.log("Tables ==> ", results);
-    });
-
 });
 
 router.get("/", (req, res) => {
 
     const query = "SELECT * FROM products ORDER BY id ASC";
 
-    // Query to fetch all data from a table in ascending order.
     connection.query(query, (err, results) => {
         if (err) throw err;
         res.render('index', { products: results });
@@ -38,24 +33,35 @@ router.get("/", (req, res) => {
 
 });
 
-router.get("/product", (req, res) => {
-    res.render('product-details');
-});
+router.get("/:id", (req, res) => {
 
-router.post('/create-product', (req, res) => {
+    console.log('Product ID : ', req.params.id);
 
-    const query = `INSERT INTO products (name,description,initial_price,final_price) VALUES(("${req.body.name}"),("${req.body.description}"),("${req.body.initialPrice}"),("${req.body.finalPrice}"))`;
+    const query = `SELECT * FROM products WHERE id = ${req.params.id}`;
 
-    // Query to store data in a table.
-    connection.query(query, (err, results) => {
-        if (err) throw err;
-        res.writeHead(302, {
-            location: "/"
-        });
-        res.end();
+    connection.query(query, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.render("product-details", { product: result[0] });
     });
 
-})
+});
+
+// router.post('/create-product', (req, res) => {
+
+//     const query = `INSERT INTO products (name,description,initial_price,final_price) VALUES(("${req.body.name}"),("${req.body.description}"),("${req.body.initialPrice}"),("${req.body.finalPrice}"))`;
+
+//     // Query to store data in a table.
+//     connection.query(query, (err, results) => {
+//         if (err) throw err;
+//         res.writeHead(302, {
+//             location: "/"
+//         });
+//         res.end();
+//     });
+
+// })
 
 app.use('/', router);
 
