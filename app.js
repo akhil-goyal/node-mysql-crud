8ab4bd7b8f67fa996cbc0d5eadcda5e1317356e3
Akhil Goyal <akhilgoyal@macbook-pro.lan>
2021-04-25 00:10:15 -0400

    Set up basic routing & view engine.

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 const express = require('express');
 const mysql = require('mysql');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const config = require('./config');
 
 const app = express();
-const config = require('./config');
+const router = express.Router();
+
+app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const connection = mysql.createConnection(config.dbConfig);
 
 connection.connect(err => {
-    if(err) {
-        throw err;
-    }
+    if (err) throw err;
     console.log("Connected to DB!");
+
+    // Query to display all tables in database.
+    connection.query("SHOW TABLES", (err, results) => {
+        if (err) throw err;
+        // console.log("Tables ==> ", results);
+    });
+    
+});
+
+router.get("/", (req,res) => {
+    res.send('Namaste!');
 })
+
+app.use('/', router);
 
 app.listen(config.serverPort, () => {
     console.log('Server is running on PORT : ', config.serverPort);
