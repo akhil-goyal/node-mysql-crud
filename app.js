9b323edb2760ffa4f5bd90b5081123ac12775c7c
Akhil Goyal <akhilgoyal@macbook-pro.lan>
2021-04-26 14:32:26 -0400

    Implemented update product functionality.

--- app.js
+++ app.js
@@ -8,20 +8,23 @@ const multer = require('multer');
 const app = express();
 const router = express.Router();
 
-// app.use(express.static('public'));
+// Setting up the static public folder.
 app.use(express.static(path.join(__dirname, "public")));
 
+// App configuration.
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const connection = mysql.createConnection(config.dbConfig);
 
+// Connecting to MySQL database.
 connection.connect(err => {
     if (err) throw err;
     console.log("Connected to DB!");
 });
 
+// Route to render landing page & fetch all products.
 router.get("/", (req, res) => {
 
     const query = "SELECT * FROM products ORDER BY id ASC";
@@ -33,6 +36,7 @@ router.get("/", (req, res) => {
 
 });
 
+// Route to display selected product & fetch product by ID.
 router.get("/:id", (req, res) => {
 
     console.log('Product ID : ', req.params.id);
@@ -48,6 +52,7 @@ router.get("/:id", (req, res) => {
 
 });
 
+// Delete a product.
 router.post("/delete-product", (req, res) => {
 
     const query = `DELETE FROM products WHERE id = ${req.body.id}`;
@@ -61,6 +66,7 @@ router.post("/delete-product", (req, res) => {
     });
 });
 
+// Storage configuration for image handling.
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'public/uploads/')
@@ -72,6 +78,7 @@ var storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+// Add a new product.
 router.post('/create-product', upload.single("image"), (req, res) => {
 
     console.dir(req.body);
@@ -85,8 +92,26 @@ router.post('/create-product', upload.single("image"), (req, res) => {
     });
 });
 
+// Update an existing product.
+router.post("/update-product", (req, res) => {
+
+    const query = `UPDATE products SET name = "${req.body.productName}", description = "${req.body.productDescription}", initial_price = "${Number(req.body.productInitialPrice)}", final_price = "${Number(req.body.productFinalPrice)}" WHERE id = "${Number(req.body.productId)}"`;
+
+    connection.query(query, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.writeHead(302, {
+            Location: "/",
+        });
+        res.end();
+    });
+});
+
+// Router initialization.
 app.use('/', router);
 
+// App initialization.
 app.listen(config.serverPort, () => {
     console.log('Server is running on PORT : ', config.serverPort);
 });
